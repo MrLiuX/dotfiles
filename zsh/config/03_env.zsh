9e259f0e17b0040f9aa6082303064855fb81c6b4
Asher Liu <i@mrliux.com>
2026-05-05 16:15:09 +0800

    Fix: env.zsh delete claude key

--- zsh/config/03_env.zsh
+++ zsh/config/03_env.zsh
@@ -8,8 +8,6 @@ export PATH="$BUN_INSTALL/bin:$PATH"
 path+=('~/scripts')
 export PATH
 
-source ~/.config/zsh/config/claudekey.zsh
-
 #export https_proxy=http://127.0.0.1:6152
 #export http_proxy=http://127.0.0.1:6152
 #export all_proxy=socks5://127.0.0.1:6153
